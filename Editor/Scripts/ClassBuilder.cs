22c4a0b25ac2b8dbd40f55768798b9c682530c4d
wagskydive <wagskydive@gmail.com>
2022-11-02 17:34:18 +0100

    rest of last commit

---
+++ Editor/Scripts/ClassBuilder.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class ClassBuilder
+{
+    public ClassBuilder()
+    {
+
+    }
+
+    public object CreateFile()
+    {
+        throw new NotImplementedException();
+    }
+}
