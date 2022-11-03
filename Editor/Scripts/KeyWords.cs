daca90aeb648bc7a5312c410e24cb4ada709dc54
wagskydive <wagskydive@gmail.com>
2022-11-03 15:54:16 +0100

    added namespace

--- Editor/Scripts/KeyWords.cs
+++ Editor/Scripts/KeyWords.cs
@@ -3,24 +3,28 @@ using System.IO;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
-internal class KeyWords
+
+namespace UnityTDDHelper
 {
-
-    public string template_test_class_keyword{get; private set;}
-
-    public string template_test_function_keyword{get; private set;}
-
-    public static KeyWords LoadKeyWords(string keywordsFilePath)
+    public class Keywords
     {
-        File.ReadAllText(keywordsFilePath);
 
-        var input = new StringReader(File.ReadAllText(keywordsFilePath));
+        public string template_test_class_keyword { get; private set; }
 
-        var deserializer = new DeserializerBuilder()
-            .WithNamingConvention(UnderscoredNamingConvention.Instance)
-            .Build();
+        public string template_test_function_keyword { get; private set; }
 
-        return deserializer.Deserialize<KeyWords>(input);
+        public static Keywords LoadKeywords(string keywordsFilePath)
+        {
+            File.ReadAllText(keywordsFilePath);
+
+            var input = new StringReader(File.ReadAllText(keywordsFilePath));
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                .Build();
+
+            return deserializer.Deserialize<Keywords>(input);
+        }
+
     }
-
 }
