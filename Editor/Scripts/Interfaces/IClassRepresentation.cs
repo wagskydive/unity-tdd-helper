8ab847d1dacbb01086b540f59ae7f334801167c9
wagskydive <wagskydive@gmail.com>
2022-11-03 22:31:33 +0100

    removed class name space, might come back

--- Editor/Scripts/Interfaces/IClassRepresentation.cs
+++ Editor/Scripts/Interfaces/IClassRepresentation.cs
@@ -5,12 +5,12 @@ namespace UnityTDDHelper
     public interface IClassRepresentation
     {
         string Name { get; }
-        string ClassNamespace { get; }
-
         string Description { get; }
 
-        IFunctionRepresentation[] Functions { get; }
         IFieldRepresentation[] Properties { get; }
+
+        IFunctionRepresentation[] Functions { get; }
+
 
     }
 }
