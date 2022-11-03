d2d4e1de01d0ba2b088c75b60371e5a01b1f119e
wagskydive <wagskydive@gmail.com>
2022-11-03 22:33:41 +0100

    refactor

--- Editor/Scripts/ClassRepresentation.cs
+++ Editor/Scripts/ClassRepresentation.cs
@@ -8,26 +8,23 @@ namespace UnityTDDHelper
         string name;
         public string Name { get => name; }
 
-        string classNamespace;
-        public string ClassNamespace { get => classNamespace; }
-
         string description;
         public string Description { get => description; }
-
-        IFunctionRepresentation[] functions;
-
-
-        public IFunctionRepresentation[] Functions { get => functions; }
 
         public IFieldRepresentation[] properties;
 
         public IFieldRepresentation[] Properties { get => properties; }
 
 
-        public ClassRepresentation(string name, string classNamespace, string description, IFunctionRepresentation[] Functions = null, IFieldRepresentation[] Properties= null)
+
+        IFunctionRepresentation[] functions;
+
+        public IFunctionRepresentation[] Functions { get => functions; }
+
+
+        public ClassRepresentation(string name, string description, IFunctionRepresentation[] Functions = null, IFieldRepresentation[] Properties = null)
         {
             this.name = name;
-            this.classNamespace = classNamespace;
             this.description = description;
             this.functions = Functions;
             this.properties = Properties;
